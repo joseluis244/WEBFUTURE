ad4bb15f4155e974f9dcb13d518dedb8508f3eb0
Jose Luis Camacho <jose.luis244@gmail.com>
2020-07-29 15:50:07 -0400

    asd

--- js/feching.js
+++ js/feching.js
@@ -1,6 +1,10 @@
 function GetPassID(carnet){
     return new Promise((Pres,Prej)=>{
+<<<<<<< HEAD
         fetch(`PHP/GetID.php?CI=${carnet}`)
+=======
+        fetch(`/PHP/GetId.php?CI=${carnet}&FN=${FN}`)
+>>>>>>> parent of d4b4e40... Id->ID
         .then(response => response.json())
         .then( (json) =>{
             if(json.existe){
