d4b4e4032ca36524e196b406de39211209723ac9
Jose Luis Camacho <jose.luis244@gmail.com>
2020-07-29 13:49:57 -0400

    Id->ID

--- js/feching.js
+++ js/feching.js
@@ -1,6 +1,6 @@
 function GetPassID(carnet,FN){
     return new Promise((Pres,Prej)=>{
-        fetch(`/PHP/GetId.php?CI=${carnet}&FN=${FN}`)
+        fetch(`/PHP/GetID.php?CI=${carnet}&FN=${FN}`)
         .then(response => response.json())
         .then( (json) =>{
             if(json.existe){
